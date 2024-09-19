2f49cb1f31f6ecd7a0116a21cfbcfa2ae902cc88
camilod529 <77802228+camilod529@users.noreply.github.com>
2024-09-19 15:34:56 +0000

    Added powershell profile and gitconfig

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,47 @@
+Import-Module -Name Terminal-Icons
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/bubblesextra.omp.json" | Invoke-Expression
+# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/tokyonight_storm.omp.json" | Invoke-Expression
+$env:VIRTUAL_ENV_DISABLE_PROMPT = 1
+
+# ----------------------
+# Aliases
+# ----------------------
+
+function o {
+    explorer.exe .
+}
+
+# ----------------------
+# Git Aliases
+# ----------------------
+function gaa {
+    git add .
+}
+
+function gitcm {
+    param (
+        [string]$message
+    )
+    git commit -m $message
+}
+
+function gpsh {
+    git push
+}
+
+function gss {
+    git status -s
+}
+
+function gs {
+    echo ""
+    echo "*********************************************"
+    echo "   DO NOT FORGET TO PULL BEFORE COMMITTING"
+    echo "*********************************************"
+    echo ""
+    git status
+}
+
+function gpl {
+    git pull
+}
